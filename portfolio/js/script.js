7bd22d38b70815e915ac7ed9ee73ccea4a497e69
Ivan Beloborodov <mail@beliv.net>
2017-10-09 11:41:01 +0300

    new project and other improvements

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -168,7 +168,7 @@ $(document).ready(function(){
 	setTimeout( function(){
 		if($("#site-type").length) {
         $(".typewrite span").typed({
-            strings: ["UX designer ", "UI evangelist ", "product designer "],
+            strings: ["UI designer ", "UX evangelist ", "Product Designer "],
             typeSpeed: 100,
             backDelay: 500,
             loop: false,
